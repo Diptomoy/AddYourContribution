e11a784d1f1a5fc92832089ec0c5bcf004bbd050
ARITRA034 <115772976+ARITRA034@users.noreply.github.com>
2022-10-17 14:53:25 +0530

    Created AlphabetStarPattern.java

---
+++ java/AlphabetStarPattern.java
@@ -0,0 +1,27 @@
+import java.util.*;  
+public class AlphabetStarPattern  
+{  
+public static void main(String[] args)  
+{  
+int i, j, n=8;  
+
+for (i = 0; i<=n; i++)   
+{  
+
+for (j = 0; j<= n / 2; j++)   
+{  
+
+if ((j == 0 || j == n / 2) && i != 0 ||  
+
+i == 0  && j != n / 2 ||   
+
+i == n / 2)   
+System.out.print("*");  
+else  
+System.out.print(" ");  
+}  
+System.out.println();  
+}  
+}  
+}  
+
