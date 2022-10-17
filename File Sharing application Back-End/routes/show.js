a775da85f4d9d6b0eec2ad8ac1ee146680ed8b11
Sumit Ghosh <97731735+nowayhome13@users.noreply.github.com>
2022-10-17 21:10:11 +0530

    File Sharing web application back-end
    Hi there . i have developed the back-end of file sharing web application using Nodejs ,  Expressja and Mongodb.

---
+++ File Sharing application Back-End/routes/show.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+//const req = require('express/lib/request');
+const File = require('../models/file');
+
+
+router.get('/:uuid', async (req,res) => {
+    try{
+        const file = await File.findOne({ uuid: req.params.uuid});
+        if(!file) {
+            return res.render('download' , {error : 'Link has been expired.'});
+        }
+        
+            return res.render('download', {
+                uuid: file.uuid,
+                fileName: file.filename,
+                fileSize: file.size,
+                downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+            })
+    } catch(err) {
+        return res.render('download' , {error : 'Something went wrong.'});
+    }
+    
+});
+
+
+
+
+
+module.exports = router;
