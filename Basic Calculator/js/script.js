18b07ae38ade7674bf1b65c384c622b1b25d98ee
vansh tyagi <113906488+vanshtyagi38@users.noreply.github.com>
2023-10-11 20:00:05 +0530

    Update script.js
    Key improvements in this code:
    
    Functions: The code is organized into functions, making it more modular and easier to understand.
    
    Error Handling: The code now includes error handling for cases where the input string might result in an error when evaluated.
    
    Improved Readability: The code is more structured and easier to read.
    
    Clear Naming: Variable and function names are chosen to be more descriptive.
    
    Commented Sections: Key parts of the code are now commented for clarity.
    
    By making these changes, your basic calculator is more effective and easier to maintain.

--- Basic Calculator/js/script.js
+++ Basic Calculator/js/script.js
@@ -1,32 +1,64 @@
-//creating text for input
-let string="";
-//selecting all elements with class button
-let buttons=document.querySelectorAll('.button');
+// Initialize the input string
+let string = "";
 
-//creating array of button
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click',(e)=>{
-        //if button is = then evaluate the value
-        if(e.target.innerHTML== '='){
-            string=eval(string);
-            document.querySelector('input').value=string;
-        }
-        //if button is AC then then clear all value
-        else if(e.target.innerHTML== 'AC'){
-            string="";
-            document.querySelector('input').value=string;
-        }
-        //if button is X then clear the last input
-        else if(e.target.innerHTML== 'DEL'){
-            string=string.substring(0, string.length - 1);;
-            document.querySelector('input').value=string;
-        }
-        //if button is other then print that on text bar
-        else{
-            console.log(e.target);
-            string=string+e.target.innerHTML;
-            document.querySelector('input').value=string;
-        }
-        
-    })
-})
+// Select all elements with class "button"
+let buttons = document.querySelectorAll('.button');
+
+// Create an array of buttons
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const inputValue = e.target.innerHTML;
+
+    // Handle the clear (AC) button
+    if (inputValue === 'AC') {
+      clearInput();
+    }
+    // Handle the delete (DEL) button
+    else if (inputValue === 'DEL') {
+      deleteLastCharacter();
+    }
+    // Handle the equals (=) button
+    else if (inputValue === '=') {
+      evaluateExpression();
+    }
+    // Handle other buttons (numbers and operators)
+    else {
+      appendInput(inputValue);
+    }
+  });
+});
+
+// Function to clear the input
+function clearInput() {
+  string = "";
+  updateInput();
+}
+
+// Function to delete the last character in the input
+function deleteLastCharacter() {
+  string = string.slice(0, -1);
+  updateInput();
+}
+
+// Function to append input
+function appendInput(input) {
+  string += input;
+  updateInput();
+}
+
+// Function to evaluate the expression and update the input
+function evaluateExpression() {
+  try {
+    string = eval(string);
+    updateInput();
+  } catch (error) {
+    // Handle any potential evaluation errors (e.g., invalid expressions)
+    string = "Error";
+    updateInput();
+  }
+}
+
+// Function to update the input element
+function updateInput() {
+  document.querySelector('input').value = string;
+}
